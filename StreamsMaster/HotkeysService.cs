3ad044991f614fbe4518c5a4d468e897e51f1637
Andre Ferreira <aadf.pt@gmail.com>
2017-04-04 22:24:50 +0100

    New keys

--- StreamsMaster/HotkeysService.cs
+++ StreamsMaster/HotkeysService.cs
@@ -4,91 +4,114 @@ using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows.Forms;
+using WindowsInput;
+using WindowsInput.Native;
 
 namespace StreamsMaster
 {
-    class HotkeysService : IDisposable
-    {
-        private List<Hotkey> _registeredHotkeys = new List<Hotkey>();
-        private VolumeService _volumeService = new VolumeService();
+	class HotkeysService : IDisposable
+	{
+		private List<Hotkey> _registeredHotkeys = new List<Hotkey>();
+		private VolumeService _volumeService = new VolumeService();
+		internal InputSimulator Device { get; set; } = new InputSimulator();
 
-        public void RegisterHotkeys(Form placeholder)
-        {
-            //this.hook.KeyIntercepted += Hook_KeyIntercepted;
-            RegisterHotkey(Keys.VolumeDown, LowerAppVolume, LowerSystemVolume, placeholder);
-            RegisterHotkey(Keys.VolumeUp, RaiseAppVolume, RaiseSystemVolume, placeholder);
-            RegisterHotkey(Keys.VolumeMute, MuteUnmuteAppVolume, MuteUnmuteSystemVolume, placeholder);
-        }
+		public void RegisterHotkeys(Form placeholder)
+		{
+			//this.hook.KeyIntercepted += Hook_KeyIntercepted;
+			RegisterHotkey(Keys.VolumeDown, LowerAppVolume, LowerSystemVolume, placeholder);
+			RegisterHotkey(Keys.VolumeUp, RaiseAppVolume, RaiseSystemVolume, placeholder);
+			RegisterHotkey(Keys.VolumeMute, MuteUnmuteAppVolume, MuteUnmuteSystemVolume, placeholder);
+			RegisterHotkey(Keys.Multiply, SendPlayPauseKey, null, placeholder);
+			RegisterHotkey(Keys.Add, SendStopKey, null, placeholder);
+			RegisterHotkey(Keys.Divide, SendPreviousTrackKey, null, placeholder);
+			RegisterHotkey(Keys.Subtract, SendNextTrackKey, null, placeholder);
+		}
 
-        private void MuteUnmuteSystemVolume(object sender, HandledEventArgs e)
-        {
-            _volumeService.MuteUnmuteSystemVolume();
-        }
+		private void SendPlayPauseKey(object sender, HandledEventArgs e)
+		{
+			Device.Keyboard.KeyPress(VirtualKeyCode.MEDIA_PLAY_PAUSE);
+		}
+		private void SendStopKey(object sender, HandledEventArgs e)
+		{
+			Device.Keyboard.KeyPress(VirtualKeyCode.MEDIA_STOP);
+		}
+		private void SendNextTrackKey(object sender, HandledEventArgs e)
+		{
+			Device.Keyboard.KeyPress(VirtualKeyCode.MEDIA_NEXT_TRACK);
+		}
+		private void SendPreviousTrackKey(object sender, HandledEventArgs e)
+		{
+			Device.Keyboard.KeyPress(VirtualKeyCode.MEDIA_PREV_TRACK);
+		}
+		private void MuteUnmuteSystemVolume(object sender, HandledEventArgs e)
+		{
+			_volumeService.MuteUnmuteSystemVolume();
+		}
 
-        private void RaiseSystemVolume(object sender, HandledEventArgs e)
-        {
-            _volumeService.RaiseSystemVolume();
+		private void RaiseSystemVolume(object sender, HandledEventArgs e)
+		{
+			_volumeService.RaiseSystemVolume();
 
-        }
+		}
 
-        private void LowerSystemVolume(object sender, HandledEventArgs e)
-        {
-            _volumeService.LowerSystemVolume();
+		private void LowerSystemVolume(object sender, HandledEventArgs e)
+		{
+			_volumeService.LowerSystemVolume();
 
-        }
+		}
 
-        private void MuteUnmuteAppVolume(object sender, HandledEventArgs e)
-        {
-            _volumeService.MuteUnmuteAppVolume();
-        }
+		private void MuteUnmuteAppVolume(object sender, HandledEventArgs e)
+		{
+			_volumeService.MuteUnmuteAppVolume();
+		}
 
-        private void RaiseAppVolume(object sender, HandledEventArgs e)
-        {
-            _volumeService.RaiseAppVolume();
-        }
+		private void RaiseAppVolume(object sender, HandledEventArgs e)
+		{
+			_volumeService.RaiseAppVolume();
+		}
 
-        private void LowerAppVolume(object sender, HandledEventArgs e)
-        {
-            _volumeService.LowerAppVolume();
-        }
+		private void LowerAppVolume(object sender, HandledEventArgs e)
+		{
+			_volumeService.LowerAppVolume();
+		}
 
-        private void RegisterHotkey(Keys key, HandledEventHandler pressedWithCtrl, HandledEventHandler pressedAlone, Form placeholder)
-        {
-            //var normalHotkey = new Hotkey
-            //{
-            //    KeyCode = key,
-            //    Control = false
-            //};
-            //normalHotkey.Pressed += pressedAlone;
+		private void RegisterHotkey(Keys key, HandledEventHandler pressedWithCtrl, HandledEventHandler pressedAlone, Form placeholder)
+		{
+			//var normalHotkey = new Hotkey
+			//{
+			//    KeyCode = key,
+			//    Control = false
+			//};
+			//normalHotkey.Pressed += pressedAlone;
 
-            var withCtrlHotkey = new Hotkey
-            {
-                KeyCode = key,
-                Control = true
-            };
-            withCtrlHotkey.Pressed += pressedWithCtrl;
+			var withCtrlHotkey = new Hotkey
+			{
+				KeyCode = key,
+				Control = true
+			};
+			withCtrlHotkey.Pressed += pressedWithCtrl;
 
-            //if (!normalHotkey.GetCanRegister(placeholder) || !withCtrlHotkey.GetCanRegister(placeholder))
-            if (!withCtrlHotkey.GetCanRegister(placeholder))
-            {
-                //TODO Send error    
-                Console.WriteLine(
-                        "Whoops, looks like attempts to register will fail or throw an exception, show an error/visual user feedback");
-                return;
-            }
-            //normalHotkey.Register(placeholder);
-            withCtrlHotkey.Register(placeholder);
+			//if (!normalHotkey.GetCanRegister(placeholder) || !withCtrlHotkey.GetCanRegister(placeholder))
+			if (!withCtrlHotkey.GetCanRegister(placeholder))
+			{
+				//TODO Send error    
+				Console.WriteLine(
+												"Whoops, looks like attempts to register will fail or throw an exception, show an error/visual user feedback");
+				return;
+			}
+			//normalHotkey.Register(placeholder);
+			withCtrlHotkey.Register(placeholder);
 
-            //_registeredHotkeys.Add(normalHotkey);
-            _registeredHotkeys.Add(withCtrlHotkey);
-        }
+			//_registeredHotkeys.Add(normalHotkey);
+			_registeredHotkeys.Add(withCtrlHotkey);
+		}
 
-        public void Dispose()
-        {
-            foreach (var hk in _registeredHotkeys.Where(hk => hk.Registered))
-            {
-                hk.Unregister();
-            }
-        }
-    }
+		public void Dispose()
+		{
+			foreach (var hk in _registeredHotkeys.Where(hk => hk.Registered))
+			{
+				hk.Unregister();
+			}
+		}
+	}
 }
--- StreamsMaster/VolumeService.cs
+++ StreamsMaster/VolumeService.cs
@@ -6,281 +6,291 @@ using Vannatech.CoreAudio.Interfaces;
 
 namespace StreamsMaster
 {
-    class VolumeService
-    {
-        private IMMDevice _speakers;
-        private IAudioEndpointVolume _aepv;
+	class VolumeService
+	{
+		private IMMDevice _speakers;
+		private IAudioEndpointVolume _aepv;
 
-        public VolumeService()
-        {
-            // get the speakers (1st render + multimedia) device
-            var deviceEnumerator = (IMMDeviceEnumerator)(new MMDeviceEnumerator());
+		public VolumeService()
+		{
+			// get the speakers (1st render + multimedia) device
+			var deviceEnumerator = (IMMDeviceEnumerator)(new MMDeviceEnumerator());
 
-            deviceEnumerator.GetDefaultAudioEndpoint(EDataFlow.eRender, ERole.eMultimedia, out _speakers);
+			deviceEnumerator.GetDefaultAudioEndpoint(EDataFlow.eRender, ERole.eMultimedia, out _speakers);
 
-            object o;
-            //retrieve the actual interface instance to retrieve the volume information from.
-            var guid = typeof(IAudioEndpointVolume).GUID;
-            _speakers.Activate(ref guid, 0, IntPtr.Zero, out o);
-            _aepv = (IAudioEndpointVolume)o;
-            Marshal.ReleaseComObject(deviceEnumerator);
-        }
+			object o;
+			//retrieve the actual interface instance to retrieve the volume information from.
+			var guid = typeof(IAudioEndpointVolume).GUID;
+			_speakers.Activate(ref guid, 0, IntPtr.Zero, out o);
+			_aepv = (IAudioEndpointVolume)o;
+			Marshal.ReleaseComObject(deviceEnumerator);
+		}
 
-        #region support
-        [ComImport]
-        [Guid("BCDE0395-E52F-467C-8E3D-C4579291692E")]
-        internal class MMDeviceEnumerator
-        {
-        }
+		#region support
 
-        internal enum EDataFlow
-        {
-            eRender,
-            eCapture,
-            eAll,
-            EDataFlow_enum_count
-        }
+		[ComImport]
+		[Guid("BCDE0395-E52F-467C-8E3D-C4579291692E")]
+		internal class MMDeviceEnumerator
+		{
+		}
 
-        internal enum ERole
-        {
-            eConsole,
-            eMultimedia,
-            eCommunications,
-            ERole_enum_count
-        }
+		internal enum EDataFlow
+		{
+			eRender,
+			eCapture,
+			eAll,
+			EDataFlow_enum_count
+		}
 
-        [Guid("A95664D2-9614-4F35-A746-DE8DB63617E6"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-        internal interface IMMDeviceEnumerator
-        {
-            int NotImpl1();
+		internal enum ERole
+		{
+			eConsole,
+			eMultimedia,
+			eCommunications,
+			ERole_enum_count
+		}
 
-            [PreserveSig]
-            int GetDefaultAudioEndpoint(EDataFlow dataFlow, ERole role, out IMMDevice ppDevice);
+		[Guid("A95664D2-9614-4F35-A746-DE8DB63617E6"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+		internal interface IMMDeviceEnumerator
+		{
+			int NotImpl1();
 
-            // the rest is not implemented
-        }
+			[PreserveSig]
+			int GetDefaultAudioEndpoint(EDataFlow dataFlow, ERole role, out IMMDevice ppDevice);
 
-        [Guid("D666063F-1587-4E43-81F1-B948E807363F"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-        internal interface IMMDevice
-        {
-            [PreserveSig]
-            int Activate(ref Guid iid, int dwClsCtx, IntPtr pActivationParams, [MarshalAs(UnmanagedType.IUnknown)] out object ppInterface);
+			// the rest is not implemented
+		}
 
-            // the rest is not implemented
-        }
+		[Guid("D666063F-1587-4E43-81F1-B948E807363F"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+		internal interface IMMDevice
+		{
+			[PreserveSig]
+			int Activate(ref Guid iid, int dwClsCtx, IntPtr pActivationParams,
+				[MarshalAs(UnmanagedType.IUnknown)] out object ppInterface);
 
-        [Guid("77AA99A0-1BD6-484F-8BC7-2C654C9A9B6F"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-        internal interface IAudioSessionManager2
-        {
-            int NotImpl1();
-            int NotImpl2();
+			// the rest is not implemented
+		}
 
-            [PreserveSig]
-            int GetSessionEnumerator(out IAudioSessionEnumerator SessionEnum);
+		[Guid("77AA99A0-1BD6-484F-8BC7-2C654C9A9B6F"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+		internal interface IAudioSessionManager2
+		{
+			int NotImpl1();
+			int NotImpl2();
 
-            // the rest is not implemented
-        }
+			[PreserveSig]
+			int GetSessionEnumerator(out IAudioSessionEnumerator SessionEnum);
 
-        [Guid("E2F5BB11-0570-40CA-ACDD-3AA01277DEE8"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-        internal interface IAudioSessionEnumerator
-        {
-            [PreserveSig]
-            int GetCount(out int SessionCount);
+			// the rest is not implemented
+		}
 
-            [PreserveSig]
-            int GetSession(int SessionCount, out IAudioSessionControl2 Session);
-        }
+		[Guid("E2F5BB11-0570-40CA-ACDD-3AA01277DEE8"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+		internal interface IAudioSessionEnumerator
+		{
+			[PreserveSig]
+			int GetCount(out int SessionCount);
 
-        [Guid("F4B1A599-7266-4319-A8CA-E70ACB11E8CD"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-        internal interface IAudioSessionControl
-        {
-            int NotImpl1();
+			[PreserveSig]
+			int GetSession(int SessionCount, out IAudioSessionControl2 Session);
+		}
 
-            [PreserveSig]
-            int GetDisplayName([MarshalAs(UnmanagedType.LPWStr)] out string pRetVal);
+		[Guid("F4B1A599-7266-4319-A8CA-E70ACB11E8CD"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+		internal interface IAudioSessionControl
+		{
+			int NotImpl1();
 
-            // the rest is not implemented
-        }
+			[PreserveSig]
+			int GetDisplayName([MarshalAs(UnmanagedType.LPWStr)] out string pRetVal);
 
-        [Guid("87CE5498-68D6-44E5-9215-6DA47EF883D8"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-        internal interface ISimpleAudioVolume
-        {
-            [PreserveSig]
-            int SetMasterVolume(float fLevel, ref Guid EventContext);
+			// the rest is not implemented
+		}
 
-            [PreserveSig]
-            int GetMasterVolume(out float pfLevel);
+		[Guid("87CE5498-68D6-44E5-9215-6DA47EF883D8"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+		internal interface ISimpleAudioVolume
+		{
+			[PreserveSig]
+			int SetMasterVolume(float fLevel, ref Guid EventContext);
 
-            [PreserveSig]
-            int SetMute(bool bMute, ref Guid EventContext);
+			[PreserveSig]
+			int GetMasterVolume(out float pfLevel);
 
-            [PreserveSig]
-            int GetMute(out bool pbMute);
-        }
-        #endregion
+			[PreserveSig]
+			int SetMute(bool bMute, ref Guid EventContext);
 
-        //public void ControlApp(uint pid, int percentage)
-        //{
-        //    foreach (var otherPid in EnumerateApplications())
-        //    {
-        //        if (otherPid != pid) continue;
-        //        // display mute state & volume level (% of master)
-        //        Console.WriteLine("Mute:" + GetApplicationMute(app));
-        //        Console.WriteLine("Volume:" + GetApplicationVolume(app));
+			[PreserveSig]
+			int GetMute(out bool pbMute);
+		}
 
-        //        // mute the application
-        //        SetApplicationMute(app, true);
+		#endregion
 
-        //        // set the volume to half of master volume (50%)
-        //        SetApplicationVolume(app, 50);
-        //    }
-        //}
+		//public void ControlApp(uint pid, int percentage)
+		//{
+		//    foreach (var otherPid in EnumerateApplications())
+		//    {
+		//        if (otherPid != pid) continue;
+		//        // display mute state & volume level (% of master)
+		//        Console.WriteLine("Mute:" + GetApplicationMute(app));
+		//        Console.WriteLine("Volume:" + GetApplicationVolume(app));
 
-        public int? GetApplicationVolume(uint pid)
-        {
-            var volume = GetVolumeObject(pid);
-            if (volume == null)
-                return null;
+		//        // mute the application
+		//        SetApplicationMute(app, true);
 
-            float level;
-            volume.GetMasterVolume(out level);
-            return Convert.ToInt32(level * 100);
-        }
+		//        // set the volume to half of master volume (50%)
+		//        SetApplicationVolume(app, 50);
+		//    }
+		//}
 
-        public bool? GetApplicationMute(uint pid)
-        {
-            var volume = GetVolumeObject(pid);
-            if (volume == null)
-                return null;
+		public int? GetApplicationVolume(uint pid)
+		{
+			var volume = GetVolumeObject(pid);
+			if (volume == null)
+				return null;
 
-            bool mute;
-            volume.GetMute(out mute);
-            return mute;
-        }
+			float level;
+			volume.GetMasterVolume(out level);
+			return Convert.ToInt32(level * 100);
+		}
 
-        public void SetApplicationVolume(uint pid, int level)
-        {
-            ISimpleAudioVolume volume = GetVolumeObject(pid);
-            if (volume == null)
-                return;
+		public bool? GetApplicationMute(uint pid)
+		{
+			var volume = GetVolumeObject(pid);
+			if (volume == null)
+				return null;
 
-            Guid guid = Guid.Empty;
-            volume.SetMasterVolume((float)level / 100, ref guid);
-        }
+			bool mute;
+			volume.GetMute(out mute);
+			return mute;
+		}
 
-        public void SetApplicationMute(uint pid, bool mute)
-        {
-            ISimpleAudioVolume volume = GetVolumeObject(pid);
-            if (volume == null)
-                return;
+		public void SetApplicationVolume(uint pid, int level)
+		{
+			ISimpleAudioVolume volume = GetVolumeObject(pid);
+			if (volume == null)
+				return;
 
-            Guid guid = Guid.Empty;
-            volume.SetMute(mute, ref guid);
-        }
+			Guid guid = Guid.Empty;
+			volume.SetMasterVolume((float)level / 100, ref guid);
+		}
 
-        public IEnumerable<uint> EnumerateApplications()
-        {
-            // activate the session manager. we need the enumerator
-            var IID_IAudioSessionManager2 = typeof(IAudioSessionManager2).GUID;
-            object o;
-            _speakers.Activate(ref IID_IAudioSessionManager2, 0, IntPtr.Zero, out o);
-            var mgr = (IAudioSessionManager2)o;
+		public void SetApplicationMute(uint pid, bool mute)
+		{
+			ISimpleAudioVolume volume = GetVolumeObject(pid);
+			if (volume == null)
+				return;
 
-            // enumerate sessions for on this device
-            IAudioSessionEnumerator sessionEnumerator;
-            mgr.GetSessionEnumerator(out sessionEnumerator);
-            int count;
-            sessionEnumerator.GetCount(out count);
+			Guid guid = Guid.Empty;
+			volume.SetMute(mute, ref guid);
+		}
 
-            for (var i = 0; i < count; i++)
-            {
-                IAudioSessionControl2 ctl;
-                sessionEnumerator.GetSession(i, out ctl);
-                uint pid;
-                ctl.GetProcessId(out pid);
-                yield return pid;
-                Marshal.ReleaseComObject(ctl);
-            }
-            Marshal.ReleaseComObject(sessionEnumerator);
-            Marshal.ReleaseComObject(mgr);
-        }
+		public IEnumerable<uint> EnumerateApplications()
+		{
+			// activate the session manager. we need the enumerator
+			var IID_IAudioSessionManager2 = typeof(IAudioSessionManager2).GUID;
+			object o;
+			_speakers.Activate(ref IID_IAudioSessionManager2, 0, IntPtr.Zero, out o);
+			var mgr = (IAudioSessionManager2)o;
 
-        private ISimpleAudioVolume GetVolumeObject(uint pid)
-        {
-            // activate the session manager. we need the enumerator
-            var IID_IAudioSessionManager2 = typeof(IAudioSessionManager2).GUID;
-            object o;
-            _speakers.Activate(ref IID_IAudioSessionManager2, 0, IntPtr.Zero, out o);
-            var mgr = (IAudioSessionManager2)o;
+			// enumerate sessions for on this device
+			IAudioSessionEnumerator sessionEnumerator;
+			mgr.GetSessionEnumerator(out sessionEnumerator);
+			int count;
+			sessionEnumerator.GetCount(out count);
 
-            // enumerate sessions for on this device
-            IAudioSessionEnumerator sessionEnumerator;
-            mgr.GetSessionEnumerator(out sessionEnumerator);
-            int count;
-            sessionEnumerator.GetCount(out count);
+			for (var i = 0; i < count; i++)
+			{
+				IAudioSessionControl2 ctl;
+				sessionEnumerator.GetSession(i, out ctl);
+				uint pid;
+				ctl.GetProcessId(out pid);
+				yield return pid;
+				Marshal.ReleaseComObject(ctl);
+			}
+			Marshal.ReleaseComObject(sessionEnumerator);
+			Marshal.ReleaseComObject(mgr);
+		}
 
-            // search for an audio session with the required name
-            // NOTE: we could also use the process id instead of the app name (with IAudioSessionControl2)
-            ISimpleAudioVolume volumeControl = null;
-            for (var i = 0; i < count; i++)
-            {
-                IAudioSessionControl2 ctl;
-                sessionEnumerator.GetSession(i, out ctl);
-                uint dn;
-                ctl.GetProcessId(out dn);
-                if (pid == dn)
-                {
-                    volumeControl = ctl as ISimpleAudioVolume;
-                    break;
-                }
-                Marshal.ReleaseComObject(ctl);
-            }
-            Marshal.ReleaseComObject(sessionEnumerator);
-            Marshal.ReleaseComObject(mgr);
-            return volumeControl;
-        }
+		private ISimpleAudioVolume GetVolumeObject(uint pid)
+		{
+			// activate the session manager. we need the enumerator
+			var IID_IAudioSessionManager2 = typeof(IAudioSessionManager2).GUID;
+			object o;
+			_speakers.Activate(ref IID_IAudioSessionManager2, 0, IntPtr.Zero, out o);
+			var mgr = (IAudioSessionManager2)o;
 
-        public void MuteUnmuteAppVolume()
-        {
-            var pid = ProcessesService.GetActiveProcessID();
-            if (!pid.HasValue) return;
-            SetApplicationMute(pid.Value, !(GetApplicationMute(pid.Value) ?? false));
-        }
+			// enumerate sessions for on this device
+			IAudioSessionEnumerator sessionEnumerator;
+			mgr.GetSessionEnumerator(out sessionEnumerator);
+			int count;
+			sessionEnumerator.GetCount(out count);
 
-        public void RaiseAppVolume()
-        {
-            var pid = ProcessesService.GetActiveProcessID();
-            if (!pid.HasValue) return;
-            var vol = GetApplicationVolume(pid.Value);
-            vol = Math.Min(100, vol.GetValueOrDefault(0) + 5);
-            SetApplicationVolume(pid.Value, vol.Value);
-        }
+			// search for an audio session with the required name
+			// NOTE: we could also use the process id instead of the app name (with IAudioSessionControl2)
+			ISimpleAudioVolume volumeControl = null;
+			for (var i = 0; i < count; i++)
+			{
+				IAudioSessionControl2 ctl;
+				sessionEnumerator.GetSession(i, out ctl);
+				uint dn;
+				ctl.GetProcessId(out dn);
+				if (pid == dn)
+				{
+					volumeControl = ctl as ISimpleAudioVolume;
+					break;
+				}
+				Marshal.ReleaseComObject(ctl);
+			}
+			Marshal.ReleaseComObject(sessionEnumerator);
+			Marshal.ReleaseComObject(mgr);
+			return volumeControl;
+		}
 
-        public void LowerAppVolume()
-        {
-            var pid = ProcessesService.GetActiveProcessID();
-            if (!pid.HasValue) return;
-            var vol = GetApplicationVolume(pid.Value);
-            vol = Math.Max(0, vol.GetValueOrDefault(0) - 5);
-            SetApplicationVolume(pid.Value, vol.Value);
-        }
+		public void MuteUnmuteAppVolume()
+		{
+			var pid = ProcessesService.GetActiveProcessID();
+			if (!pid.HasValue) return;
+			SetApplicationMute(pid.Value, !(GetApplicationMute(pid.Value) ?? false));
+		}
 
-        public void MuteUnmuteSystemVolume()
-        {
-            bool muteState;
-            _aepv.GetMute(out muteState);
-            _aepv.SetMute(!muteState, new System.Guid());
-        }
+		public void RaiseAppVolume()
+		{
+			var pid = ProcessesService.GetActiveProcessID();
+			if (!pid.HasValue) return;
+			var vol = GetApplicationVolume(pid.Value);
+			vol = Math.Min(100, vol.GetValueOrDefault(0) + 5);
+			SetApplicationVolume(pid.Value, vol.Value);
+		}
 
-        public void RaiseSystemVolume()
-        {
-            _aepv.VolumeStepUp(new System.Guid());
-        }
+		public void LowerAppVolume()
+		{
+			var pid = ProcessesService.GetActiveProcessID();
+			if (!pid.HasValue) return;
+			var vol = GetApplicationVolume(pid.Value);
+			vol = Math.Max(0, vol.GetValueOrDefault(0) - 5);
+			SetApplicationVolume(pid.Value, vol.Value);
+		}
 
-        public void LowerSystemVolume()
-        {
+		public void MuteUnmuteSystemVolume()
+		{
+			bool muteState;
+			_aepv.GetMute(out muteState);
+			_aepv.SetMute(!muteState, new System.Guid());
+		}
 
-            _aepv.VolumeStepDown(new System.Guid());
-        }
-    }
+		public void RaiseSystemVolume()
+		{
+			_aepv.SetMute(false, new System.Guid());
+			for (var i = 0; i < 6; i++)
+			{
+				_aepv.VolumeStepUp(new System.Guid());
+			}
+		}
+
+		public void LowerSystemVolume()
+		{
+			_aepv.SetMute(false, new System.Guid());
+			for (var i = 0; i < 4; i++)
+			{
+				_aepv.VolumeStepDown(new System.Guid());
+			}
+		}
+	}
 }
